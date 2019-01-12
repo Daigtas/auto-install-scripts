c5f816666a8b1a81d59834cfde60dffb3ce66685
Daigtas <44111535+Daigtas@users.noreply.github.com>
2019-01-12 10:33:52 +0200

    this is a small file change
    changed from curl to wget and made it even more silent

--- install.sh
+++ install.sh
@@ -5,25 +5,26 @@ os=$(cat /etc/os-release | grep ID | cut -d= -f2 | head -1)
 
 curl_check ()
 {
-  echo "Checking for curl..."
-  if command -v curl > /dev/null; then
-    echo "Detected curl..."
+# changed curl to a wget for less errors
+  echo "Checking for wget..."
+  if command -v wget > /dev/null; then
+    echo "Detected wget..."
   else
-    echo "Installing curl..."
-
+    echo "Installing wget..."
+#removed the >/dev/null direction becouse the debian apt supports silent mode with an -qq switch
     if [ "${os}" = "debian" ]; then
-        apt-get -y update &> /dev/null
-        apt-get -q -y install curl &> /dev/null
+        apt-get -qq update
+        apt-get -qq -y install wget -y
     elif [ "${os}" = "ubuntu" ]; then 
-        apt-get -y update &> /dev/null
-        apt-get install -q -y curl &> /dev/null
+        apt-get -qq update
+        apt-get -qq install wget -y
     elif [ "${os}" = "centos" ]; then 
-        yum -q -y update &> /dev/null
-        yum -q -y install curl &> /dev/null
+        yum -qq update
+        yum -qq install wget
     fi
 
     if [ "$?" -ne "0" ]; then
-      echo "Unable to install curl! Your base system has a problem; please check your default OS's package repositories because curl should work." >> /dev/stderr
+      echo "Unable to install wget! Your base system has a problem; please check your default OS's package repositories because curl should work." >> /dev/stderr
       echo "Repository installation aborted." >> /dev/stderr
       exit 1
     fi
@@ -49,7 +50,10 @@ curl_check
 echo
 echo
 echo "Downloading installator for your operating system..."
-curl -sL $script --output /tmp/gameap-install.sh &> /dev/null
+# Changed curl to wget and the command gets a bit differend. 
+# First cd to a dir then download the script and then rename to a needed script (gameap-install.sh).
+# and ofcorse there is no file presence check used. I can add that one if you want to.
+cd /tmp/ | wget -q $script | mv install-en.sh gameap-install.sh
 chmod +x /tmp/gameap-install.sh
 
 echo
